39db1261d1488fae0edb0deb984aaef2d86cdfad
raphplt <raphael.plassart@gmail.com>
2024-10-29 21:27:36 +0100

    Refactor next.config.mjs to use fileURLToPath and path modules

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,8 @@
+import { fileURLToPath } from "url";
 import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
