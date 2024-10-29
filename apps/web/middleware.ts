532ce082c05367b8c2a02313e3b373415904b479
raphplt <raphael.plassart@gmail.com>
2024-10-29 22:59:54 +0100

    Refactor middleware.ts to use ES modules syntax and add session decryption

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,35 +1,50 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import { decrypt } from "@/lib/session";
+import { TextEncoder } from "util";
+import { jwtVerify } from "jose";
 
 const protectedRoutes = ["/dashboard", "/profile"];
 const publicRoutes = ["/auth/login", "/auth/register", "/"];
 
+const secretKey = process.env.SESSION_SECRET;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+async function decrypt(session: string | undefined = "") {
+    try {
+        const { payload } = await jwtVerify(session, encodedKey, {
+            algorithms: ["HS256"],
+        });
+        return payload;
+    } catch (error) {
+        return null;
+    }
+}
+
 export default async function middleware(req: NextRequest) {
-	// Récupérer le chemin de l'URL
-	const path = req.nextUrl.pathname;
-	const isProtectedRoute = protectedRoutes.includes(path);
-	const isPublicRoute = publicRoutes.includes(path);
+    // Récupérer le chemin de l'URL
+    const path = req.nextUrl.pathname;
+    const isProtectedRoute = protectedRoutes.includes(path);
+    const isPublicRoute = publicRoutes.includes(path);
 
-	// Récupérer le cookie de session
-	const cookie = cookies().get("session")?.value;
-	const session = await decrypt(cookie);
+    // Récupérer le cookie de session
+    const cookie = cookies().get("session")?.value;
+    const session = await decrypt(cookie);
 
-	if (isProtectedRoute && !session?.userId) {
-		return NextResponse.redirect(new URL("auth/login", req.nextUrl));
-	}
+    if (isProtectedRoute && !session?.userId) {
+        return NextResponse.redirect(new URL("auth/login", req.nextUrl));
+    }
 
-	if (
-		isPublicRoute &&
-		session?.userId &&
-		!req.nextUrl.pathname.startsWith("/profile")
-	) {
-		return NextResponse.redirect(new URL("/profile", req.nextUrl));
-	}
+    if (
+        isPublicRoute &&
+        session?.userId &&
+        !req.nextUrl.pathname.startsWith("/profile")
+    ) {
+        return NextResponse.redirect(new URL("/profile", req.nextUrl));
+    }
 
-	return NextResponse.next();
+    return NextResponse.next();
 }
 
 export const config = {
-	matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+    matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
