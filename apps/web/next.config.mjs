8ffd3099909185753da26d8ef11e72f6b5f77c08
raphplt <raphael.plassart@gmail.com>
2024-10-29 21:23:08 +0100

    Refactor build configurations for Vercel deployment

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,22 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
 		domains: ["res.cloudinary.com"],
+	},
+	webpack: (config) => {
+		config.resolve.alias = {
+			...config.resolve.alias,
+			"@app": path.resolve(__dirname, "app"),
+			"@/components": path.resolve(__dirname, "components"),
+			"@/hooks": path.resolve(__dirname, "hooks"),
+			"@/lib": path.resolve(__dirname, "lib"),
+			"@/utils": path.resolve(__dirname, "utils"),
+			"@public": path.resolve(__dirname, "public"),
+			"@/contexts": path.resolve(__dirname, "contexts"),
+		};
+		return config;
 	},
 };
 
