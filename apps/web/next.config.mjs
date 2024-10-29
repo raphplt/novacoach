12ac67cf0412b715f649fcff477c1f59e8e94755
raphplt <raphael.plassart@gmail.com>
2024-10-29 21:30:57 +0100

    Refactor next.config.mjs to remove unused code and optimize webpack configuration

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,26 +1,7 @@
-import { fileURLToPath } from "url";
-import path from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
 		domains: ["res.cloudinary.com"],
-	},
-	webpack: (config) => {
-		config.resolve.alias = {
-			...config.resolve.alias,
-			"@app": path.resolve(__dirname, "app"),
-			"@/components": path.resolve(__dirname, "components"),
-			"@/hooks": path.resolve(__dirname, "hooks"),
-			"@/lib": path.resolve(__dirname, "lib"),
-			"@/utils": path.resolve(__dirname, "utils"),
-			"@public": path.resolve(__dirname, "public"),
-			"@/contexts": path.resolve(__dirname, "contexts"),
-		};
-		return config;
 	},
 };
 
