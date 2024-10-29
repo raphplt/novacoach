af6a002fef65e2b9361c97ef42f3a86d74e808d1
raphplt <raphael.plassart@gmail.com>
2024-10-29 22:36:36 +0100

    au revoir pnpm

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,21 +1,7 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	distDir: "./dist",
 	images: {
 		domains: ["res.cloudinary.com"],
-	},
-	webpack: (config) => {
-		config.resolve.alias = {
-			...config.resolve.alias,
-			"@": path.resolve(__dirname),
-		};
-		return config;
 	},
 };
 
