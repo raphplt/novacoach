101a1b3ce0508ab06b403f1279c01bd78d1e5e7e
raphplt <raphael.plassart@gmail.com>
2024-10-29 23:21:47 +0100

    cscqx

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,35 +1,38 @@
 import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
 import { decrypt } from "./lib/session";
 
 const protectedRoutes = ["/dashboard", "/profile"];
 const publicRoutes = ["/auth/login", "/auth/register", "/"];
 
-export default async function middleware(req: NextRequest) {
-	// Récupérer le chemin de l'URL
-	const path = req.nextUrl.pathname;
-	const isProtectedRoute = protectedRoutes.includes(path);
-	const isPublicRoute = publicRoutes.includes(path);
+export async function middleware(req: NextRequest) {
+    const path = req.nextUrl.pathname;
+    const isProtectedRoute = protectedRoutes.includes(path);
+    const isPublicRoute = publicRoutes.includes(path);
+const cookie = req.cookies.get("session")?.value;
+    let session;
+    try {
+        session = await decrypt(cookie);
+    } catch (error) {
+        console.error("Erreur de décryptage du cookie:", error);
+        session = null;
+    }
 
-	// Récupérer le cookie de session
-	const cookie = cookies().get("session")?.value;
-	const session = await decrypt(cookie);
+    if (isProtectedRoute && !session?.userId) {
+        return NextResponse.redirect(new URL("/auth/login", req.url));
+    }
 
-	if (isProtectedRoute && !session?.userId) {
-		return NextResponse.redirect(new URL("auth/login", req.nextUrl));
-	}
+    if (
+        isPublicRoute &&
+        session?.userId &&
+        !req.nextUrl.pathname.startsWith("/profile")
+    ) {
+        return NextResponse.redirect(new URL("/profile", req.url));
+    }
 
-	if (
-		isPublicRoute &&
-		session?.userId &&
-		!req.nextUrl.pathname.startsWith("/profile")
-	) {
-		return NextResponse.redirect(new URL("/profile", req.nextUrl));
-	}
-
-	return NextResponse.next();
+    return NextResponse.next();
 }
 
 export const config = {
-	matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+	matcher: ["/((?!api|_next/static|_next/image|.\.png$).)"], 
+	
 };
