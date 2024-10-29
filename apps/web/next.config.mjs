57f3e4913a1915b415a20af0683c78c9cb925331
raphplt <raphael.plassart@gmail.com>
2024-10-29 21:59:21 +0100

    tesg

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	reactStrictMode: true,
+	output: "export", // Si vous voulez générer une application statique
+	distDir: "./dist", // Change le répertoire de build en './dist/'
 	images: {
 		domains: ["res.cloudinary.com"],
+	},
+	experimental: {
+		turbotrace: false,
 	},
 };
 
