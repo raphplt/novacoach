821642cd35fc0ba2cbf92143396e9061f39105a2
raphaeltkorp <raphael@tkorp.com>
2024-11-17 21:50:45 +0100

    Refactor UserProfile component: simplify userSportPrograms rendering and improve code structure

--- apps/web/app/coach/dashboard/students/profil/[id]/page.tsx
+++ apps/web/app/coach/dashboard/students/profil/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { UserType } from "type/user";
 import useFetchData from "@hooks/useFetchData";
 import { getLastValueFromUserDetails } from "@utils/functions/getLastValueFromUserDetails";
@@ -22,10 +22,8 @@ const UserProfile = () => {
 		}
 	}, [userData]);
 
-
 	if (!user) return <div className="text-center mt-4">Loading...</div>;
 	getLastValueFromUserDetails(user);
-
 
 	return (
 		<div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-blue-100 to-indigo-200 py-10">
@@ -120,63 +118,42 @@ const UserProfile = () => {
 					</div>
 				)}
 
-				{user.userSportPrograms &&
-					user.userSportPrograms.length > 0 && (
-						<div className="mt-6 p-4 bg-indigo-50 rounded-lg border border-indigo-200">
-							<h3 className="text-xl font-semibold text-indigo-600 mb-2">
-								Programmes de sport
-							</h3>
-
-							{user?.userSportPrograms.map((program, index) => (
-								<div
-									key={index}
-									className="mb-4"
-								>
-									<p className="mb-2 text-gray-700">
-										<span className="font-semibold">
-											Program Name:
-										</span>{" "}
-										{program.sportProgram.name}
-									</p>
-									<p className="mb-2 text-gray-700">
-										<span className="font-semibold">
-											Difficulty:
-										</span>{" "}
-										{program.sportProgram.difficulty}
-									</p>
-									<p className="mb-2 text-gray-700">
-										<span className="font-semibold">
-											Frequency:
-										</span>{" "}
-										{program.sportProgram.frequency}{" "}
-										sessions/week
-									</p>
-									<p className="mb-2 text-gray-700">
-										<span className="font-semibold">
-											Duration:
-										</span>{" "}
-										{program.sportProgram.duration} days
-									</p>
-									<p className="mb-2 text-gray-700">
-										<span className="font-semibold">
-											Start Date:
-										</span>{" "}
-										{new Date(
-											program.startDate,
-										).toLocaleDateString()}
-									</p>
-									<p className="mb-2 text-gray-700">
-										<span className="font-semibold">
-											End Date:
-										</span>{" "}
-										{new Date(
-											program.endDate,
-										).toLocaleDateString()}
-									</p>
-								</div>
-							))}
-						</div>
-					)}
+				{user.userSportPrograms && (
+					<div className="mt-6 p-4 bg-indigo-50 rounded-lg border border-indigo-200">
+						<h3 className="text-xl font-semibold text-indigo-600 mb-2">
+							Programme de sport
+						</h3>
+						<p className="mb-2 text-gray-700">
+							<span className="font-semibold">Program Name:</span>{" "}
+							{user.userSportPrograms.sportProgram.name}
+						</p>
+						<p className="mb-2 text-gray-700">
+							<span className="font-semibold">Difficulty:</span>{" "}
+							{user.userSportPrograms.sportProgram.difficulty}
+						</p>
+						<p className="mb-2 text-gray-700">
+							<span className="font-semibold">Frequency:</span>{" "}
+							{user.userSportPrograms.sportProgram.frequency}{" "}
+							sessions/week
+						</p>
+						<p className="mb-2 text-gray-700">
+							<span className="font-semibold">Duration:</span>{" "}
+							{user.userSportPrograms.sportProgram.duration} days
+						</p>
+						<p className="mb-2 text-gray-700">
+							<span className="font-semibold">Start Date:</span>{" "}
+							{new Date(
+								user.userSportPrograms.startDate,
+							).toLocaleDateString()}
+						</p>
+						<p className="mb-2 text-gray-700">
+							<span className="font-semibold">End Date:</span>{" "}
+							{new Date(
+								user.userSportPrograms.endDate,
+							).toLocaleDateString()}
+						</p>
+					</div>
+				)}
 			</div>
 		</div>
 	);
